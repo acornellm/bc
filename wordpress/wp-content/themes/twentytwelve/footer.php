277ae69b49269668e8ab0cc6ef1f92d9be93e09d
acornellm <adam.c.myers@gmail.com>
2013-09-15 23:15:01 -0600

    adding footer images

--- wordpress/wp-content/themes/twentytwelve/footer.php
+++ wordpress/wp-content/themes/twentytwelve/footer.php
@@ -17,6 +17,10 @@
             <p>
                 <a href="http://www.goldenmarketingllc.com" target="_blank">Website Design by Golden Marketing LLC</a>
             </p>
+			<p>
+                <a href="http://www.visitgolden.com" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/visitgolden.png" /></a>
+                <a href="http://www.golden.com/buygolden" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/buygolden.png" /></a>
+            </p>
         </div><!-- .site-info -->
     </footer><!-- #colophon -->
 </div><!-- #page -->
